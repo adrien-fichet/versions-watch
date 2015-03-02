9b170333af229790d0b8125cfaaee04746f85934
sniksnp <sniksnp@gmail.com>
2015-03-02 20:36:25 +0100

    Factorize load_versions.js functions Add item icons

--- src/main/resources/static_files/load_versions.js
+++ src/main/resources/static_files/load_versions.js
@@ -1,31 +1,48 @@
 $(function() {
-  loadGitVersion();
+  var singleVersions = ['git', 'debian', 'subversion'];
+  var multipleVersions = [['jetbrains', ['youtrack', 'idea']]];
+  createVersionTableRows(singleVersions, multipleVersions);
+  loadSingleVersions(singleVersions, 0, [loadMultipleVersions, multipleVersions, null]);
 });
 
-function loadGitVersion() {
-  $.getJSON("/git", function(data) {
-    $("#git_version").text(data.git);
-    loadJetbrainsVersions();
+function createVersionTableRows(singleVersions, multipleVersions) {
+  for (var i=0; i < singleVersions.length; i++) {
+    $('#versions').append(createVersionRow(singleVersions[i]));
+  }
+  for (var i=0; i < multipleVersions.length; i++) {
+    for (var j=0; j < multipleVersions[i][1].length; j++) {
+      $('#versions').append(createVersionRow(multipleVersions[i][1][j]));
+    }
+  }
+}
+
+function createVersionRow(name) {
+  var formattedName = name.charAt(0).toUpperCase() + name.slice(1);
+  return '<tr>' +
+    '<td class="icon"><img src="/im/' + name + '.jpg"/></td>' +
+    '<td>' + formattedName + '</td>' +
+    '<td id="' + name + '">loading...</td>' +
+    '</tr>';
+}
+
+function loadSingleVersions(versions, index, args) {
+  $.getJSON('/' + versions[index], function(data) {
+    $('#' + versions[index]).text(data[versions[index]]);
+    if (index < versions.length - 1) {
+      loadSingleVersions(versions, index + 1, args);
+    } else {
+      args[0](args[1], 0, args[2]);
+    }
   });
 }
 
-function loadJetbrainsVersions() {
-  $.getJSON("/jetbrains", function(data) {
-    $("#youtrack_version").text(data.youtrack);
-    $("#idea_version").text(data.idea);
-    loadDebianVersion();
-  });
-}
-
-function loadDebianVersion() {
-  $.getJSON("/debian", function(data) {
-    $("#debian_version").text(data.debian);
-    loadSubversionVersion();
-  });
-}
-
-function loadSubversionVersion() {
-  $.getJSON("/subversion", function(data) {
-    $("#subversion_version").text(data.subversion);
+function loadMultipleVersions(versions, index, args) {
+  $.getJSON('/' + versions[index][0], function(data) {
+    for (var i=0; i < versions[index][1].length; i++) {
+      $('#' + versions[index][1][i]).text(data[versions[index][1][i]]);
+    }
+    if (index < versions.length - 1) {
+      loadMultipleVersions(versions, index + 1, args);
+    }
   });
 }
