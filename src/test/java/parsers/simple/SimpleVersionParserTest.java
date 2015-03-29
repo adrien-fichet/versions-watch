aed9e1d31a2277309d3a6f18ea22cf4f5d5ea5af
sniksnp <sniksnp@gmail.com>
2015-03-29 23:00:50 +0200

    Move SimpleVersionParser test files to the same directory

--- src/test/java/parsers/simple/SimpleVersionParserTest.java
+++ src/test/java/parsers/simple/SimpleVersionParserTest.java
@@ -8,75 +8,74 @@ import static org.junit.Assert.assertEquals;
 
 public class SimpleVersionParserTest {
 
-    public void testSimpleVersionParser(String documentOriginPath, VersionParserConfiguration conf,
-                                        String expectedVersion) throws Exception {
-        String documentOrigin = getClass().getResource(documentOriginPath).getPath();
+    public void testSimpleVersionParser(String testFileName, VersionParserConfiguration conf, String expectedVersion) throws Exception {
+        String documentOrigin = getClass().getResource("/parsers/simple/" + testFileName).getPath();
         SimpleVersionParser apacheVersionParser = new FakeSimpleVersionParser(conf, documentOrigin);
         assertEquals(expectedVersion, apacheVersionParser.parseVersion());
     }
 
     @Test
     public void testApacheSimpleVersionParser() throws Exception {
-        testSimpleVersionParser("/parsers/apache/Apache.html", Conf.apacheVersionParserConfiguration, "2.4.12");
+        testSimpleVersionParser("Apache.html", Conf.apacheVersionParserConfiguration, "2.4.12");
     }
 
     @Test
     public void testDebianSimpleVersionParser() throws Exception {
-        testSimpleVersionParser("/parsers/debian/Debian.html", Conf.debianVersionParserConfiguration, "7.8");
+        testSimpleVersionParser("Debian.html", Conf.debianVersionParserConfiguration, "7.8");
     }
 
     @Test
     public void testEclipseSimpleVersionParser() throws Exception {
-        testSimpleVersionParser("/parsers/eclipse/Eclipse.html", Conf.eclipseVersionParserConfiguration, "Luna SR2 (4.4.2)");
+        testSimpleVersionParser("Eclipse.html", Conf.eclipseVersionParserConfiguration, "Luna SR2 (4.4.2)");
     }
 
     @Test
     public void testGitSimpleVersionParser() throws Exception {
-        testSimpleVersionParser("/parsers/git/Git.html", Conf.gitVersionParserConfiguration, "2.3.1");
+        testSimpleVersionParser("Git.html", Conf.gitVersionParserConfiguration, "2.3.1");
     }
 
     @Test
     public void testJavaSimpleVersionParser() throws Exception {
-        testSimpleVersionParser("/parsers/java/JavaSE.html", Conf.javaVersionParserConfiguration, "8u40");
+        testSimpleVersionParser("JavaSE.html", Conf.javaVersionParserConfiguration, "8u40");
     }
 
     @Test
     public void testJenkinsSimpleVersionParser() throws Exception {
-        testSimpleVersionParser("/parsers/jenkins/Jenkins.html", Conf.jenkinsVersionParserConfiguration, "1.606");
+        testSimpleVersionParser("Jenkins.html", Conf.jenkinsVersionParserConfiguration, "1.606");
     }
 
     @Test
     public void testMysqlSimpleVersionParser() throws Exception {
-        testSimpleVersionParser("/parsers/mysql/Mysql.html", Conf.mysqlVersionParserConfiguration, "5.6");
+        testSimpleVersionParser("Mysql.html", Conf.mysqlVersionParserConfiguration, "5.6");
     }
 
     @Test
     public void testNetbeansSimpleVersionParser() throws Exception {
-        testSimpleVersionParser("/parsers/netbeans/Netbeans.html", Conf.netbeansVersionParserConfiguration, "8.0.2");
+        testSimpleVersionParser("Netbeans.html", Conf.netbeansVersionParserConfiguration, "8.0.2");
     }
 
     @Test
     public void testPhpSimpleVersionParser() throws Exception {
-        testSimpleVersionParser("/parsers/php/Php.html", Conf.phpVersionParserConfiguration, "5.6.7");
+        testSimpleVersionParser("Php.html", Conf.phpVersionParserConfiguration, "5.6.7");
     }
 
     @Test
     public void testPuppetSimpleVersionParser() throws Exception {
-        testSimpleVersionParser("/parsers/puppet/Puppet.html", Conf.puppetVersionParserConfiguration, "3.7.5");
+        testSimpleVersionParser("Puppet.html", Conf.puppetVersionParserConfiguration, "3.7.5");
     }
 
     @Test
     public void testSublimeTextSimpleVersionParser() throws Exception {
-        testSimpleVersionParser("/parsers/sublime_text/SublimeText.html", Conf.sublimeTextVersionParserConfiguration, "2.0.2");
+        testSimpleVersionParser("SublimeText.html", Conf.sublimeTextVersionParserConfiguration, "2.0.2");
     }
 
     @Test
     public void testSubversionSimpleVersionParser() throws Exception {
-        testSimpleVersionParser("/parsers/subversion/Subversion.html", Conf.subversionVersionParserConfiguration, "1.8.11");
+        testSimpleVersionParser("Subversion.html", Conf.subversionVersionParserConfiguration, "1.8.11");
     }
 
     @Test
     public void testTomcatSimpleVersionParser() throws Exception {
-        testSimpleVersionParser("/parsers/tomcat/Tomcat.html", Conf.tomcatVersionParserConfiguration, "8.0.21");
+        testSimpleVersionParser("Tomcat.html", Conf.tomcatVersionParserConfiguration, "8.0.21");
     }
 }
